58b3f60c3143450c47d0dd423695d23859fa1dc2
salehgomaa60 <salehgomaazzb@gmail.com>
2023-10-04 13:56:39 -0700

    45

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -7,13 +7,15 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
+ * for ALX project
  */
-typedef struct listints
+typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-} listint;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
