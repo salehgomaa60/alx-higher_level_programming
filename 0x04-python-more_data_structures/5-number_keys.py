f3eb9e4258ee1b31968bc8df12887acda803117a
salehgomaa60 <salehgomaaazzb@gmail.com>
2024-01-25 13:03:22 -0800

    458

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    list_ord = list(a_dictionary.keys())
-    list_ord.sort()
-    for i in list_ord:
-        print("{}: {}".format(i, a_dictionary.get(i)))
+def number_keys(a_dictionary):
+    num = 0
+    list_keys = list(a_dictionary.keys())
+
+    for i in list_keys:
+        num += 1
+
+    return (num)
