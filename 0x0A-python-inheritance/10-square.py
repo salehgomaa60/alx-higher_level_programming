efc24bfe37dc9130a02d633695db013a79ed8417
salehgomaa60 <salehgomaazzb@gmail.com>
2024-04-17 19:49:02 -0700

    not allowed to import

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Defines a Rectangle subclass Square."""
-Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -13,5 +12,4 @@ class Square(Rectangle):
             size (int): The size of the new square.
         """
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
