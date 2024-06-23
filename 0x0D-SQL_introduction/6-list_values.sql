aa96cab368f7cf25623e4e34ea904f953fd12675
salehgomaa60 <salehgomaazzb@gmail.com>
2024-06-23 12:19:00 -0700

    listing and inserting a row

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- List all rows of  table first_table in my mysqlserver.
+SELECT * FROM `first_table`;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- inserts a new row  into the table first table id = 89 and name = bestschool
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
