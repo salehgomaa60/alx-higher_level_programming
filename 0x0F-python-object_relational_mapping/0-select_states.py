afa66d75fe7ee3904eb7135602064e7ab56738e1
salehgomaa60 <salehgomaazzb@gmail.com>
2024-06-23 16:01:01 -0700

    trying to list the states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" listing the  states """
+
+import MySQLdb
+import sys
+
+
+def main():
+    db = MySQLdb.connect(
+          user=sys.argv[1],
+          host="localhost",
+          port=3306,
+          password=sys.argv[2],
+          db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
