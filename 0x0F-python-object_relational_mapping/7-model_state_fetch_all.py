6dcf6ab0808a9253dfabcacff4b3e7b3316e46b0
salehgomaa60 <salehgomaazzb@gmail.com>
2024-06-23 16:18:36 -0700

    model stste

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""List states from mysqldb"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+def db_engine():
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{user}:{password}@localhost:3306/{db}'
+        )
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    db_engine()
