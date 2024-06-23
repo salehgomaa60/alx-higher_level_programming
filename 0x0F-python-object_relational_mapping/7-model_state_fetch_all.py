121f904c08a5e4b48fd651ea8dec7ca508ba0a05
salehgomaa60 <salehgomaazzb@gmail.com>
2024-06-23 16:42:51 -0700

    trying to implement order by

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,31 +1,20 @@
 #!/usr/bin/python3
-"""List all the  states from mysqldb in ascending order by id """
-
-
-from sqlalchemy import create_engine, asc
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+""" listing all states in asc order fetch all to table in database
+"""
 import sys
-
-
-def db_engine():
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}'
-        )
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    states = session.query(State).order_by(asc(state.id)).all()
-    for state in states:
-        print(f"{state.id}: {state.name}")
-
-    session.close()
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    db_engine()
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
