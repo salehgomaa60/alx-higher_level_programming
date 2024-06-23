412c41bf028bce31484685e0e78fac6c4fe8630c
salehgomaa60 <salehgomaazzb@gmail.com>
2024-06-23 16:28:31 -0700

    sa

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""List states from mysqldb"""
+"""List all the  states from mysqldb in ascending order by id """
 
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
 
 def db_engine():
-    user = sys.argv[1]
+    username = sys.argv[1]
     password = sys.argv[2]
-    db = sys.argv[3]
+    database = sys.argv[3]
 
     engine = create_engine(
-        f'mysql+mysqldb://{user}:{password}@localhost:3306/{db}'
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}'
         )
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).all()
+    states = session.query(State).order_by(asc(state.id)).all()
     for state in states:
         print(f"{state.id}: {state.name}")
         print("\n")
