9fb5b114f87a400c88fb90276e5199ab5013181d
salehgomaa60 <salehgomaazzb@gmail.com>
2024-06-23 16:53:21 -0700

    trying to implement order by 2

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" listing all states in asc order fetch all to table in database
+"""Start link class to table in database
 """
 import sys
 from model_state import Base, State
@@ -8,16 +8,10 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    user = sys.argv[1]
-    password = sys.argv[2]
-    db = sys.argv[3]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
-         print(f"{state.id}: {state.name}")
-         session.close()
-         
+    for state in session.query(State).order_by(State.id):
+        print(state.id, state.name, sep=": ")        
